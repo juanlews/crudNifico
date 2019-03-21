adeedc6c5c6efa559186bc32f5585940128f46bf
juanlews <juanlews@hotmail.com>
2019-03-21 00:36:13 -0300

    initial structure

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+struct data{
+
+    int product_id;
+    string name;
+    string description;
+    float price;
+    int quantity;
+    string type;
+
+};
+
+void create(){
+     FILE *arq;
+   //  arq fopen();
+}
+
+void read(){
+}
+
+void update(){
+}
+
+void deleted(){
+}
+
+int main()
+{
+    cout << "" << endl;
+    return 0;
+}
