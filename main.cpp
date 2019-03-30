4a2c92aa7add315be75d3e471dc7d854d7985d1b
juanlews <juanlews@hotmail.com>
2019-03-30 13:42:58 -0300

    create product

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-#include <string>
+
 #include <stdlib.h>
+
+#include <stdio.h>
+
+#include <iostream>
+
+#include <string>
 
 using namespace std;
 
@@ -19,8 +25,71 @@ data * openAndParse(){
 
 }
 
+void opc_create(){
+    bool repeat = true;
+    data toCreate;
+    toCreate.name = "";
+
+    while( repeat  ){
+
+        printf("Nome do produto: ");
+
+        cin >> toCreate.name;
+        cout <<  toCreate.name;
+
+        if(toCreate.name != ""){
+            cout <<  toCreate.name;
+            repeat = false;
+        }
+    }
+    repeat = true;
+    toCreate.description = "";
+    while(repeat){
+
+        printf("\nDescrição: ");
+        cin >> toCreate.description;
+        cout <<  toCreate.description;
+        if(toCreate.description != ""){
+            cout <<  toCreate.description;
+            repeat = false;
+        }
+    }
+    //price
+    repeat = true;
+    toCreate.price = 0.0;
+    while(repeat){
+
+        printf("\nDescrição: ");
+        cin >> toCreate.price;
+        repeat = false;
+    }
+
+    //qty
+    repeat = true;
+    toCreate.quantity = 0;
+    while(repeat){
+
+        printf("\nDescrição: ");
+        cin >> toCreate.quantity;
+
+        repeat = false;
+
+    }
+    //type
+    repeat = true;
+    toCreate.type = "";
+    while(repeat){
+
+        printf("\nDescrição: ");
+        cin >> toCreate.type;
+        if(toCreate.type!= ""){
+            repeat = false;
+        }
+    }
+}
+
 void create(){
-    FILE *arq;
+
 
 }
 
@@ -37,7 +106,8 @@ void deleted(){
 }
 
 int main(){
-
+    setlocale(LC_ALL,"portuguese");
     cout << "" << endl;
+    opc_create();
     return 0;
 }
