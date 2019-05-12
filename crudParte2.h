118d2e253993affea916cd750683945f5910b0ad
juanlews <juanlews@hotmail.com>
2019-05-12 00:07:20 -0300

    funçoes basicas para o carregamento do indice

--- crudParte2.h
+++ crudParte2.h
@@ -3,6 +3,7 @@
 //struct categoria;
 
 int indexCategoria = 0;
+int categoriaValidas = 0;
 
 struct categoria {
     int categoria_id;
@@ -45,6 +46,48 @@ void create_categoria(int id = -1){
 
 //return aux;
 
+}
+
+void updateCategoria(categoria aux){
+
+    //data aux = create();
+    FILE *arq = fopen("dataCategoria.txt", "a+");
+    if (arq == NULL) {
+        cout<<"\n\tErro na Leitura/Gravacao do arquivo!";
+    }else{
+        fprintf(arq, "%i\t%s\n",aux.categoria_id, aux.nomeCategoria);
+    }
+    fclose(arq);
+}
+
+void listar_categorias(categoria * toShow){
+    for (int i = 0; i > indexCategoria; i++){
+        printf("%i - %s", toShow[i].categoria_id, toShow[i].nomeCategoria);
+    }
+}
+
+categoria *openFileCategorias(int n){
+    categoriaValidas = 0;
+    categoria * toLoad = (categoria *)calloc(n, sizeof(categoria));
+    if(toLoad == NULL){
+        cout << "n�o h� memoria suficiente";
+    } else {
+        FILE *arq = fopen("dataCategoria.txt", "r+");
+        int i = 0;
+        if(arq){
+            while( !feof(arq) ){
+                fscanf( arq, "%i\t%[^\n]\n", toLoad[i].categoria_id, toLoad[i].nomeCategoria);
+                if(!(toLoad[i].categoria_id <= -1)){//id negativo serve como lapide
+                    categoriaValidas++;
+                    i++;
+                }else{
+                    //Imprimir_pesquisa(toLoad[i]);
+                }
+            }
+        }
+        fclose(arq);
+    }
+    return toLoad;
 }
 
 #endif // CELULA_H_INCLUDED
