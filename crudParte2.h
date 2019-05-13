4b14561cc142caf86ae844d2db563f2485cd390d
juanlews <juanlews@hotmail.com>
2019-05-12 22:02:41 -0300

    CRUD para categoria

--- crudParte2.h
+++ crudParte2.h
@@ -28,7 +28,7 @@ int indiceCategoria (){  //Retorna o valor do indice
     return x;
 }
 
-void create_categoria(int id = -1){
+categoria create_categoria(int id = -1){
 
     categoria aux;
     if(id >= 0){
@@ -44,7 +44,7 @@ void create_categoria(int id = -1){
     gets(aux.nomeCategoria);
     cout << endl;
 
-//return aux;
+    return aux;
 
 }
 
@@ -58,6 +58,12 @@ void updateCategoria(categoria aux){
         fprintf(arq, "%i\t%s\n",aux.categoria_id, aux.nomeCategoria);
     }
     fclose(arq);
+}
+
+void Imprimir_pesquisaCategoria(categoria toShow){
+
+    printf("\n\nProduct_id: %i\n Nome: %s \n\n",toShow.categoria_id, toShow.nomeCategoria);
+
 }
 
 void listar_categorias(categoria * toShow){
@@ -81,13 +87,64 @@ categoria *openFileCategorias(int n){
                     categoriaValidas++;
                     i++;
                 }else{
-                    //Imprimir_pesquisa(toLoad[i]);
+                    Imprimir_pesquisaCategoria(toLoad[i]);
                 }
             }
         }
         fclose(arq);
     }
     return toLoad;
+}
+
+categoria *alteraFileCategorias(int n, int id, bool opcExclusao = false){
+    categoriaValidas = 0;
+    bool finded = false;
+    categoria * toLoad = (categoria *)calloc(n, sizeof(categoria));
+
+    if(toLoad == NULL){
+        cout << "não há memoria suficiente";
+    } else {
+        FILE *arq = fopen("dataCategoria.txt", "r+");
+        int i = 0;
+        if(arq){
+            while( !feof(arq) ){
+                fscanf( arq, "%i\t%[^\n]\n", toLoad[i].categoria_id, toLoad[i].nomeCategoria);
+                if(!(toLoad[i].categoria_id <= -1)){//id negativo serve como lapide
+                    if(toLoad[i].categoria_id == id){
+                        //anda no arquivo
+                        finded = true;
+                        cout << "Produto pra ser alterado:\n";
+                        Imprimir_pesquisaCategoria(toLoad[i]);
+                        if(!opcExclusao){
+                            toLoad[i] = create_categoria(id);
+                            toLoad[i].categoria_id = id;
+                        }
+                        if(opcExclusao){
+                            toLoad[i].categoria_id = -2;
+                        }
+                    }
+                    categoriaValidas++;
+                    i++;
+                } else {
+                    i++;
+                }
+            }
+        }
+        fclose(arq);
+        FILE *arqNovo = fopen("dataCategoria.txt", "w+");
+        int a = 0;
+        while(a < i){
+            fprintf(arqNovo, "%i\t%s\t%s\t%f\t%i\t%s\n",
+                        toLoad[a].categoria_id, toLoad[a].nomeCategoria);
+                a++;
+            }
+            fclose(arqNovo);
+        }
+
+        if(!finded){
+            cout<<"Categoria não encontrada.\n\n";
+        }
+        return toLoad;
 }
 
 #endif // CELULA_H_INCLUDED
