d93140269321ac216aaf5c1999cc480312cc2863
davigdc <36992852+davigdc@users.noreply.github.com>
2019-05-12 21:29:10 -0300

    Estrutura do menu pronta

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <string>
 #include <stdlib.h>
 #include <locale.h>
-//#include <ProcedimentosHash.h>
+
 using namespace std;
+#include "crudCliente.h";
 
 int index = 0, validos = 0;
 
@@ -288,77 +289,224 @@ data *alteraFile(int n, int id, bool opcExclusao = false){
 int main(){
     setlocale(LC_ALL, "");
 
-    int menu, idToFind = -1;
-    data *v, aux;
+    int menu, idToFind = -1, subMenu;
 
-    int fator=32;    // potencia de 2 abaixo do tamanho total (total= 12800 -> potencia de 2 mais proxima 65536 -> numero primo abaixo 65521), como Sedgewick sugere
+	data *v, aux;
+	//categoria *categorias, auxCategoria;
+	cliente *clientes, auxCliente;
+	//compra *compras, auxCompra;
+
+	int fator=32;
     ListaH **TabelaHash = (ListaH **)calloc(fator,sizeof(ListaH *));
     InicializarHash(TabelaHash, fator);
 
-    indice();
-    v = openFile(index);
-
     do{
-        printf("\tMenu:\n1 - Inclusão\n2 - Alteração\n3 - Exclusão\n4 - Pesquisa\n5 - Sair\n-->Entrada: ");
-        cin>>menu;
+        printf("====> Menu <====\n1- Inclusao\n2- Alteracao\n3- Exclusao\n4- Pesquisa\n5- Sair\n-->Entrada: ");
+        cin >> menu;
         cout << endl;
         fflush(stdin);
         if(menu == 5){
             break;
         }
         if(menu < 1 || menu > 5){
-            cout<<"\t\tOpcao invalida, digite novamente...\n";
+            cout << "\tOpcao invalida, digite novamente...\n";
         }
 
-        v = 0;
+            //Inicia cruds
+		v = 0;
         indice();
         v = openFile(index);
-        InicializarHash(TabelaHash, fator);
-        for(int i=0; i< validos; i++){
+		InicializarHash(TabelaHash, fator);
+        for(int i = 0; i < validos; i++){
             InserirHash(TabelaHash, fator, v[i]);
         }
 
+		// categorias = 0;
+		// indiceCategoria();
+		// categorias = openFileCategoria();
+
+		clientes = 0;
+		indiceCliente();
+		clientes = openFileCliente(indexCliente);
+
+		// compras = 0;
+		// indiceCompras();
+		// compras = openFileCompras();
+
         switch(menu){
             case 1: // Inclusao
-            {
-                update(create());
-            }
+
+            	cout << "Digite 1 para incluir um produto, 2 para categoria, 3 para cliente ou 4 para compra: ";
+            	cin >> subMenu;
+            	fflush(stdin);
+
+				if(subMenu < 1 || subMenu > 4){
+					cout << "\tOpcao invalida, digite novamente...\n";
+				}
+
+            	switch(subMenu){
+            		case 1:	//Produto
+						update(create());
+					break;
+
+            		case 2:	//Categoria
+						//updateCategoria(create_categoria);
+					break;
+
+            		case 3:	//Cliente
+						updateCliente(create_cliente());
+					break;
+
+            		case 4:	//Compra
+						//upadateCompras(create_compra);
+					break;
+
+					}
             break;
 
             case 2: //Alteracao
-                printf("Alteração no ID: ");
-                cin >> idToFind;
-                fflush(stdin);
-                alteraFile(index, idToFind);
 
+				cout << "Digite 1 para alterar um produto, 2 para categoria, 3 para cliente ou 4 para compra: ";
+            	cin >> subMenu;
+            	fflush(stdin);
+
+				if(subMenu < 1 || subMenu > 4){
+					cout << "\tOpcao invalida, digite novamente...\n";
+				}
+
+            	switch(subMenu){
+            		case 1:	//Produto
+						printf("(Produto)Alteracao no ID: ");
+						cin >> idToFind;
+						fflush(stdin);
+						alteraFile(index, idToFind);
+            		break;
+
+            		case 2:	//Categoria
+						// printf("(Categoria)Alteracao no ID: ");
+						// cin >> idToFind;
+						// fflush(stdin);
+						// alteraFileCategoria(indexCategoria, idToFind);
+            		break;
+
+            		case 3:	//Cliente
+						printf("(Cliente)Alteracao no ID: ");
+						cin >> idToFind;
+						fflush(stdin);
+						alteraFileCliente(indexCliente, idToFind);
+            		break;
+
+            		case 4:	//Compra
+						// printf("(Compra)Alteracao no ID: ");
+						// cin >> idToFind;
+						// fflush(stdin);
+						// alteraFileCompra(indexCompra, idToFind);
+            		break;
+
+				}
             break;
 
             case 3: //Exclusao
-                cout<<"Exclusãono ID: ";
-                cin >> idToFind;
-                fflush(stdin);
-                alteraFile(index, idToFind, true);
 
+            	cout << "Digite 1 para excluir um produto, 2 para categoria, 3 para cliente ou 4 para compra: ";
+            	cin >> subMenu;
+            	fflush(stdin);
+
+				if(subMenu < 1 || subMenu > 4){
+					cout << "\tOpcao invalida, digite novamente...\n";
+				}
+
+            	switch(subMenu){
+            		case 1:	//Produto
+						printf("(Produto)Exclusao no ID: ");
+						cin >> idToFind;
+						fflush(stdin);
+						alteraFile(index, idToFind, true);
+					break;
+
+            		case 2:	//Categoria
+						// printf("(Categoria)Exclusao no ID: ");
+						// cin >> idToFind;
+						// fflush(stdin);
+						// alteraFileCategoria(indexCategoria, idToFind, true);
+					break;
+
+            		case 3:	//Cliente
+						printf("(Cliente)Exclusao no ID: ");
+						cin >> idToFind;
+						fflush(stdin);
+						alteraFileCliente(indexCliente, idToFind, true);
+					break;
+
+            		case 4:	//Compras
+						// printf("(Compra)Exclusao no ID: ");
+						// cin >> idToFind;
+						// fflush(stdin);
+						// alteraFileCompra(indexCompra, idToFind, true);
+					break;
+
+				}
             break;
 
             case 4://Pesquisa
-                cout<<"Pesquisa no ID: ";
-                cin >> idToFind;
 
-                aux=PesquisarHash(TabelaHash, fator, idToFind);
-                //Imprimir_pesquisa(aux);
-                //cout<<endl<<aux.product_id<<endl<<endl;
+			  	cout << "Digite 1 para pesquisar um produto, 2 para categoria, 3 para cliente ou 4 para compra: ";
+            	cin >> subMenu;
+            	fflush(stdin);
 
-                if(aux.product_id == -1){
-                    printf("Produto não encontrado.\n");
-                } else {
-                    Imprimir_pesquisa(aux);
-                }
+				if(subMenu < 1 || subMenu > 4){
+					cout << "\tOpcao invalida, digite novamente...\n";
+				}
 
+            	switch(subMenu){
+            		case 1:  //Produto
+						printf("(Produto)Pesquisa no ID: ");
+						cin >> idToFind;
+						aux = PesquisarHash(TabelaHash, fator, idToFind);
+						if(aux.product_id == -1){
+							printf("Produto não encontrado.\n");
+						} else {
+							Imprimir_pesquisa(aux);
+						}
+            		break;
+
+            		case 2:	//Categoria
+						// printf("(Categoria)Pesquisa no ID: ");
+						// cin >> idToFind;
+						// auxCategoria = PesquisarCategoria(categorias, idToFind);
+						// if(aux.categoria_id == -1){
+						// 	printf("Produto não encontrado.\n");
+						// } else {
+						// 	Imprimir_pesquisaCategoria(auxCategoria);
+						// }
+            		break;
+
+            		case 3:	//Cliente
+						printf("(Cliente)Pesquisa no ID: ");
+						cin >> idToFind;
+						auxCliente = PesquisarCliente(clientes, idToFind);
+						if(auxCliente.cliente_id == -1){
+							printf("Produto não encontrado.\n");
+						} else {
+							Imprimir_pesquisaCliente(auxCliente);
+						}
+            		break;
+
+            		case 4:	//Compra
+						// printf("(Compra)Pesquisa no ID: ");
+						// cin >> idToFind;
+						// auxCompra = PesquisarCompra(compras, idToFind);
+						// if(aux.compra_id == -1){
+						// 	printf("Produto não encontrado.\n");
+						// } else {
+						// 	Imprimir_pesquisaCompra(auxCompra);
+						// }
+            		break;
+
+				}
             break;
         }
-
-    }while(menu != 5);
+    } while (menu != 5);
 
 return 0;
 }
